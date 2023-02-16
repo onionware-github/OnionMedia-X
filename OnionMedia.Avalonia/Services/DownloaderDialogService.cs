ca5a8ece432c7129c8fe7290ffabf3cb35c63f79
Jaden <91136545+onionware-github@users.noreply.github.com>
2023-02-16 15:52:49 +0100

    Ported PlaylistSelectorDialog

--- OnionMedia.Avalonia/Services/DownloaderDialogService.cs
+++ OnionMedia.Avalonia/Services/DownloaderDialogService.cs
@@ -15,6 +15,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAvalonia.UI.Controls;
+using OnionMedia.Avalonia.Views.Dialogs;
+using OnionMedia.Core.ViewModels.Dialogs;
 using YoutubeExplode.Videos;
 
 namespace OnionMedia.Services
@@ -23,10 +25,9 @@ namespace OnionMedia.Services
     {
         public async Task<IEnumerable<IVideo>> ShowPlaylistSelectorDialogAsync(IEnumerable<IVideo> videosFromPlaylist)
         {
-            return null;
-            //PlaylistSelectorDialog dlg = new(videosFromPlaylist) { XamlRoot = UIResources.XamlRoot };
-            //if (await dlg.ShowAsync() != ContentDialogResult.Primary) return Array.Empty<IVideo>();
-            //return dlg.ViewModel.Videos.Where(v => v.IsSelected);
+            PlaylistSelectorDialog dlg = new(videosFromPlaylist);
+            if (await dlg.ShowAsync() != ContentDialogResult.Primary) return Array.Empty<IVideo>();
+            return ((PlaylistSelectorViewModel)dlg.DataContext).Videos.Where(v => v.IsSelected);
         }
     }
 }
---
+++ OnionMedia.Avalonia/Views/Dialogs/PlaylistSelectorDialog.axaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+using FluentAvalonia.UI.Controls;
+using OnionMedia.Core.ViewModels.Dialogs;
+using YoutubeExplode.Videos;
+
+namespace OnionMedia.Avalonia.Views.Dialogs;
+
+public partial class PlaylistSelectorDialog : ContentDialog, IStyleable
+{
+    Type IStyleable.StyleKey => typeof(ContentDialog);
+    
+    /// <summary> FOR AVALONIA XAML ONLY, DO NOT USE MANUALLY!</summary>
+    public PlaylistSelectorDialog() {}
+
+    public PlaylistSelectorDialog(IEnumerable<IVideo> videos)
+    {
+        DataContext = new PlaylistSelectorViewModel(videos);
+        this.InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+    
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+        var btn = e.NameScope.Find<Button>("PrimaryButton");
+        btn?.Classes.Add("AccentButtonStyle");
+    }
+}
