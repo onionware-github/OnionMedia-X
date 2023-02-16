3cecff51c659bcfadcd2bc4ecff677a6e99e118a
Jaden <91136545+onionware-github@users.noreply.github.com>
2023-02-16 20:35:17 +0100

    Show filename on MediaPage

---
+++ OnionMedia.Avalonia/Converters/RemoveFilenameExtensionConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Avalonia.Data.Converters;
+
+namespace OnionMedia.Avalonia.Converters;
+
+public class RemoveFilenameExtensionConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not string path) return value;
+        return Path.GetFileNameWithoutExtension(path);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
